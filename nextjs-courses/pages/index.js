4c8d2ea7a3e8313ffa801b41e4a31875add0037a
vihahuynh <huynhviha1703@gmail.com>
2023-08-04 18:46:39 +0700

    getStaticProps function

--- nextjs-courses/pages/index.js
+++ nextjs-courses/pages/index.js
@@ -1,9 +1,30 @@
-const HomePage = () => {
+import fs from "fs/promises";
+import path from "path";
+
+const HomePage = (props) => {
   return (
-    <div>
-      <h1>The Home Page</h1>
-    </div>
+    <ul>
+      {props.products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
   );
 };
 
 export default HomePage;
+
+export async function getStaticProps(context) {
+  const filePath = path.join(process.cwd(), "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 60,
+    notFound: false,
+    redirect: {
+      destination: "/no-data",
+    },
+  };
+}
